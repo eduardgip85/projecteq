20ba1c078d5dae3d3050dc630541ca96b8c8fee9
Edu <eduardgip85@gmail.com>
2020-05-05 18:05:23 +0200

    cards

--- plantilla_vue/public/js/app.js
+++ plantilla_vue/public/js/app.js
@@ -2087,6 +2087,9 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   props: ["pregunta"],
   data: function data() {
@@ -79179,7 +79182,11 @@ var render = function() {
                     key: resposta.id_resposta,
                     attrs: { value: resposta.text_resposta }
                   },
-                  [_vm._v(_vm._s(resposta.text_resposta))]
+                  [
+                    _vm._v(
+                      _vm._s(resposta.text_resposta) + "\n                    "
+                    )
+                  ]
                 )
               }),
               1
@@ -91694,8 +91701,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\VARIS\Cep\DAW2B\PROJECTE\Quizz\projecteq\plantilla_vue\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\VARIS\Cep\DAW2B\PROJECTE\Quizz\projecteq\plantilla_vue\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\projecteq\plantilla_vue\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\projecteq\plantilla_vue\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- plantilla_vue/resources/views/quiz/menu.blade.php
+++ plantilla_vue/resources/views/quiz/menu.blade.php
@@ -7,32 +7,42 @@
 @section('principal')
 
 <div class="row menucenter">
-    <div class="card-deck" >
-        <div class="card text-center" id="card-boostrap">
+  
+  <div class="card-deck" >
+
+      <a onclick="" class="card text-center" id="card-boostrap">
+        
           <img class="card-img-top" src="/img/tournament.jpg" alt="Card image cap">
           <div class="card-body">          
             <button type="button" class="btn boto-secondari" action>
               <h5 class="mt-2">Tournament Quiz</h5>
             </button>
           </div>
-        </div>
-      <div class="card text-center" id="card-boostrap">
-        <img class="card-img-top" src="/img/game.jpg" alt="Card image cap">
-        <div class="card-body">        
-          <button type="button" class="btn boto-secondari"  onclick="window.location='{{ url('/game') }}'" action>
-            <h5 class="mt-2">Game Quiz</h5>
-          </button>
-        </div>
-      </div>
-  
-      <div class="card text-center" id="card-boostrap" action>
-        <img class="card-img-top" src="/img/rankss.jpg" alt="Card image cap">
-        <div class="card-body">
-          <button type="button" class="btn  boto-secondari" action>
-            <h5 class="mt-2">What is your rank?</h5>
-          </button>
-        </div>
-      </div>
+        
+      </a>
+
+      <a onclick="window.location='{{ url('/game') }}'" class="card text-center" id="card-boostrap">
+        
+          <img class="card-img-top" src="/img/game.jpg" alt="Card image cap">
+          <div class="card-body">        
+            <button type="button" class="btn boto-secondari" action>
+              <h5 class="mt-2">Game Quiz</h5>
+            </button>
+          </div>
+        
+      </a>
+        
+
+      <a onclick="" class="card text-center" id="card-boostrap">
+        
+          <img class="card-img-top" src="/img/rankss.jpg" alt="Card image cap">
+          <div class="card-body">
+            <button type="button" class="btn  boto-secondari" action>
+              <h5 class="mt-2">What is your rank?</h5>
+            </button>
+          </div>
+        
+      </a>
     
 </div>
 
