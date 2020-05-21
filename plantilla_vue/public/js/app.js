fcb46b2330831081d4d6a1cc07b3eec814139c35
Edu <eduardgip85@gmail.com>
2020-05-21 18:01:55 +0200

    menus

--- plantilla_vue/public/js/app.js
+++ plantilla_vue/public/js/app.js
@@ -91876,8 +91876,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\VARIS\Cep\DAW2B\PROJECTE\Quizz\projecteq\plantilla_vue\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\VARIS\Cep\DAW2B\PROJECTE\Quizz\projecteq\plantilla_vue\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\projecteq\plantilla_vue\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\projecteq\plantilla_vue\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- plantilla_vue/resources/views/main_menu.blade.php
+++ plantilla_vue/resources/views/main_menu.blade.php
@@ -6,22 +6,33 @@
 
 @section('principal')
 
-    <div class="row">
-        
-        <a href="{{ url('/quiz') }}">
-            <button class="btn bg-primary">
-                CSGO
-            </button>
-        </a>        
-        
-        <a href=" {{ url('/milionari') }}">
-            <button class="btn bg-seconadry">
-                MIllonari
-            </button>
-        </a>        
-
+<div class="row menucenter">
+  
+    <div class="card-deck" >
+  
+        <a onclick="window.location='{{ url('/quiz') }}'" class="card text-center" id="card-boostrap">
+          
+            <img class="card-img-top" src="/img/tournament.jpg" alt="Card image cap">
+            <div class="card-body">          
+              <button type="button" class="btn boto-secondari" action>
+                <h5 class="mt-2">CSGO</h5>
+              </button>
+            </div>
+          
+        </a>
+  
+        <a onclick="window.location='{{ url('/milionari') }}'" class="card text-center" id="card-boostrap">
+          
+            <img class="card-img-top" src="/img/millonario.jpg" alt="Card image cap">
+            <div class="card-body">        
+              <button type="button" class="btn boto-secondari" action>
+                <h5 class="mt-2">Millonario</h5>
+              </button>
+            </div>
+          
+        </a>
+      
     </div>
-
 
     <footer class="" id="footer">
 
